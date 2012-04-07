e4f7444f13be06d6395a6aca8afd459f36182e54
fereli <blacksmith@vip.qq.com>
2012-04-07 16:09:05 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+PROJECT_CP="."
+for aJar in  `ls target/dependency`
+do	
+    PROJECT_CP="target/dependency/$aJar:$PROJECT_CP"
+done
+LIBS="-classpath target/classes:$PROJECT_CP"
+MAIN_CLASS="org.jboss.netty.example.echo.EchoServer"
+LOG_CONFIG="-Dlog4j.configuration=file:./log4j.properties"
+java $LIBS $MAIN_CLASS $@ 
+echo "$!" >server-pid.txt
