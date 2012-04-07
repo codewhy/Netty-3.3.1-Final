4cda752347a88dd3e660709355cf6ac61a72522f
fereli <blacksmith@vip.qq.com>
2012-04-07 16:09:59 +0800

    add a start shell

--- start.sh
+++ start.sh
@@ -7,4 +7,3 @@ LIBS="-classpath target/classes:$PROJECT_CP"
 MAIN_CLASS="org.jboss.netty.example.echo.EchoServer"
 LOG_CONFIG="-Dlog4j.configuration=file:./log4j.properties"
 java $LIBS $MAIN_CLASS $@ 
-echo "$!" >server-pid.txt
